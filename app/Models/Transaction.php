9e8dc79e0c061eb4e663d8529362768b9907a5d3
William <william.tomenh@gmail.com>
2022-04-17 21:23:06 -0300

    feat(Models): create transaction model.

---
+++ app/Models/Transaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'origin_bank',
+        'origin_agency',
+        'origin_account',
+        'destiny_bank',
+        'destiny_agency',
+        'destiny_account',
+        'amount',
+        'date',
+    ];
+}
