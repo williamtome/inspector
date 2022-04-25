44e96d9052ea8472c9cb4d016fbb3400224659d2
William <william.tomenh@gmail.com>
2022-04-25 20:59:46 -0300

    feat(CsvService): move business rule to CsvService class.

---
+++ app/Http/Services/CsvService/CsvService.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Services\CsvService;
+
+use App\Http\Repositories\Csv\CsvRepository;
+use App\Http\Repositories\Importation\ImportationRepository;
+use App\Http\Repositories\Transaction\TransactionRepository;
+use App\Models\Importation;
+use Carbon\Carbon;
+
+class CsvService
+{
+    private const FIRSTCSVROW = 0;
+    private bool|string $csvPath;
+    private \Generator|array $rows;
+    private CsvRepository $csvRepository;
+    private TransactionRepository $repository;
+    private ImportationRepository $importationRepository;
+    private Carbon $transactionDate;
+    private Importation $importation;
+
+    public function __construct(
+        string $pathFile,
+        TransactionRepository $repository,
+        CsvRepository $csvRepository,
+        ImportationRepository $importationRepository,
+    ) {
+        $this->csvPath = $pathFile;
+        $this->repository = $repository;
+        $this->csvRepository = $csvRepository;
+        $this->importationRepository = $importationRepository;
+    }
+
+    public function handle()
+    {
+        $this->rows = $this->csvRepository->read($this->csvPath);
+
+        foreach ($this->rows as $row) {
+            if (!$row) {
+                break;
+            }
+
+            if (array_search("", $row)) {
+                continue;
+            }
+
+            $date = Carbon::parse($row[7]);
+
+            if ($this->isFirstRowOfCSVFile()) {
+                $this->transactionDate = $date;
+
+                if ($this->repository->existForDate($this->transactionDate)) {
+                    return redirect()->back()
+                        ->with('error', 'Já existe transações para esta data!');
+                }
+
+                $this->importation = $this->importationRepository->store([
+                    'transactions_date' => $this->transactionDate->toDateString(),
+                ]);
+
+                $this->repository->store(
+                    $this->getCsvPayload($row, $this->importation)
+                );
+
+                continue;
+            }
+
+            if ($this->transactionDate->toDateString() != $date->toDateString()) {
+                continue;
+            }
+
+            $this->repository->store($this->getCSVPayload($row, $this->importation));
+        }
+    }
+
+    private function getCsvPayload($data, $importation): array
+    {
+        return [
+            'origin_bank' => $data[0],
+            'origin_agency' => $data[1],
+            'origin_account' => $data[2],
+            'destiny_bank' => $data[3],
+            'destiny_agency' => $data[4],
+            'destiny_account' => $data[5],
+            'amount' => $data[6],
+            'date' => $data[7],
+            'importation_id' => $importation->id,
+        ];
+    }
+
+    private function isFirstRowOfCSVFile(): bool
+    {
+        return $this->rows->key() === self::FIRSTCSVROW;
+    }
+}
