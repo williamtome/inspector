fffc6724d19c2e6e0e419c46e4d2928154851daa
William <william.tomenh@gmail.com>
2022-04-28 21:17:38 -0300

    refactor(routes): add auth and dashboard routes.

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\TransactionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/home');
+    return redirect('/login');
 });
 
-Route::get('/home', [TransactionsController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+Route::get('/home', [TransactionsController::class, 'index']);
 Route::post('/upload', [TransactionsController::class, 'upload'])
     ->name('upload');
 
+require __DIR__.'/auth.php';
