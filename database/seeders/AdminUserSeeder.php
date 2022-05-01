33188ebb32c303755bd2a326f0e08e833be95738
William <william.tomenh@gmail.com>
2022-05-01 16:04:28 -0300

    feat(Seeders): create admin user.

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@email.com.br',
+            'password' => bcrypt('123999'),
+        ]);
+    }
+}
