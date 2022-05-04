74a3a2c09b9d06e9660c5441087c74f59c7cc2f5
William <william.tomenh@gmail.com>
2022-05-03 21:58:51 -0300

    refactor(views/navigation): change translate logout name.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Sair') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
