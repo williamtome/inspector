9bc2da42d4467f8d2992e1c95af6880cbf46d653
William <william.tomenh@gmail.com>
2022-05-06 19:42:59 -0300

    refactor(TransactionsController): redirect to importation details page.

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -7,6 +7,7 @@ use App\Http\Repositories\Importation\ImportationRepository;
 use App\Http\Repositories\Transaction\TransactionRepository;
 use App\Http\Requests\UploadRequest;
 use App\Http\Services\Csv\CsvService;
+use App\Models\Importation;
 use Illuminate\Support\Facades\DB;
 
 class TransactionsController extends Controller
@@ -25,11 +26,9 @@ class TransactionsController extends Controller
         $this->importationRepository = $importationRepository;
     }
 
-    public function index()
+    public function show(Importation $importation)
     {
-        $importations = $this->importationRepository->getAllByTransactionsDate();
-
-        return view('upload.form', ['importations' => $importations]);
+        return view('upload.details', ['importation' => $importation]);
     }
 
     public function upload(UploadRequest $request)
