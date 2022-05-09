063665fe7c7dd7a630650a3794a80ed44566a1cb
William <william.tomenh@gmail.com>
2022-05-09 08:16:20 -0300

    refactor(UserFactory): use email and password default.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,13 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $name = $this->faker->name();
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $name,
+            'email' => strtolower(str_replace([' ', '.'], '-', $name)) . '@teste.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt(env('USER_PASSWORD')), // password
             'remember_token' => Str::random(10),
         ];
     }
