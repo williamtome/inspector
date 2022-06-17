6cc7f6da4ba02574ee93ea25f5d5ba19bbc2ca6b
William <william.tomenh@gmail.com>
2022-06-17 18:20:08 -0300

    refactor(User): add HasMany type returned.

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function importations()
+    public function importations(): HasMany
     {
         return $this->hasMany('App\Models\Importation');
     }
