6fa881a541bc3b543a34416f7aa102325311fd29
William <william.tomenh@gmail.com>
2022-06-17 18:19:23 -0300

    feat(User): add query scopes.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ActiveScope;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -46,5 +50,17 @@ class User extends Authenticatable
     public function importations()
     {
         return $this->hasMany('App\Models\Importation');
+    }
+
+    public function scopeType(Builder $query): Builder
+    {
+        return Auth::user()->name !== 'Admin'
+            ? $query->where('name', '<>', 'Admin')
+            : $query;
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new ActiveScope);
     }
 }
